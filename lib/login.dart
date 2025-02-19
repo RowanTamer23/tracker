b25e9620ced513b41cbc3412136ffad43325d18a
RowanTamer23 <rowan.tamer232001@gmail.com>
2025-02-19 14:07:54 +0200

    login overview

--- lib/login.dart
+++ lib/login.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class LogIn extends StatelessWidget {
-  const LogIn({super.key});
+class LogIn extends StatefulWidget {
+  @override
+  LogIn({super.key});
+  _LogInState createState() => _LogInState();
+}
 
+class _LogInState extends State<LogIn> {
+  bool _isObscured = true;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
-          primarySwatch: Colors.blue,
+          primarySwatch: Colors.grey,
         ),
         home: Scaffold(
             backgroundColor: Color.fromRGBO(175, 120, 99, 1),
@@ -27,25 +32,64 @@ class LogIn extends StatelessWidget {
                           ),
                           SizedBox(height: 20),
                           TextField(
+                            cursorColor: Color.fromARGB(255, 218, 187, 167),
                             decoration: InputDecoration(
-                              labelText: 'Email',
-                              hintText: 'example@example.com',
-                            ),
+                                focusedBorder: UnderlineInputBorder(
+                                    // Underline when clicked (focused)
+                                    borderSide: BorderSide(
+                                        color:
+                                            Color.fromARGB(255, 218, 187, 167),
+                                        width: 2)),
+                                labelText: 'Email',
+                                labelStyle: TextStyle(
+                                    color: Color.fromRGBO(0, 0, 0, 150)),
+                                hintText: 'example@example.com',
+                                hintStyle: TextStyle(
+                                    color: Color.fromRGBO(0, 0, 0, 150))),
                           ),
                           TextField(
+                            obscureText: _isObscured,
+                            cursorColor: Color.fromARGB(255, 218, 187, 167),
                             decoration: InputDecoration(
-                              labelText: 'password',
-                              hintText: 'Pass@123',
-                            ),
+                                focusedBorder: UnderlineInputBorder(
+                                    // Underline when clicked (focused)
+                                    borderSide: BorderSide(
+                                        color:
+                                            Color.fromARGB(255, 218, 187, 167),
+                                        width: 2)),
+                                labelText: 'password',
+                                suffixIcon: IconButton(
+                                  icon: Icon(
+                                    _isObscured
+                                        ? Icons.visibility_off
+                                        : Icons.visibility,
+                                  ),
+                                  onPressed: () {
+                                    setState(() {
+                                      _isObscured = !_isObscured;
+                                    });
+                                  },
+                                ),
+                                labelStyle: TextStyle(
+                                    color: Color.fromRGBO(0, 0, 0, 150)),
+                                hintText: 'Pass@123',
+                                hintStyle: TextStyle(
+                                    color: Color.fromRGBO(0, 0, 0, 150))),
                           ),
-                          SizedBox(height: 20),
+                          SizedBox(height: 30),
                           TextButton(
-                              onPressed: () {},
-                              child: Text('login',
-                                  style: GoogleFonts.sigmar(
-                                    color: Color.fromARGB(255, 218, 187, 167),
-                                    fontSize: 20,
-                                  ))),
+                            onPressed: () {},
+                            style: TextButton.styleFrom(
+                                backgroundColor:
+                                    Color.fromARGB(255, 218, 187, 167),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(50))),
+                            child: Text('login',
+                                style: GoogleFonts.sigmar(
+                                  color: Color.fromRGBO(175, 120, 99, 1),
+                                  fontSize: 23,
+                                )),
+                          )
                         ])))));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(debugShowCheckedModeBanner: false, home: LogIn());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: LogIn());
   }
 }
